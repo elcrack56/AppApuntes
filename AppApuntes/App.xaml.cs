e969463fad53a82029b50429cfb7dec34beb369f
PC0050 <PC0050@DESKTOP-7A5EIBM>
2024-12-01 18:24:13 -0500

    Agregar archivos de proyecto.

---
+++ AppApuntes/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppApuntes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppApuntes/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppApuntes
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
